c2d8194a04f72904a88bd69d71c0c1676f7ee4e3
arcade_kappa <gkappa@outlands.top>
2024-05-18 23:28:02 +0800

    Make it coremod

--- src/main/java/minefantasy/mfr/core/MixinLoader.java
+++ src/main/java/minefantasy/mfr/core/MixinLoader.java
@@ -1,16 +1,41 @@
 package minefantasy.mfr.core;
 
 import zone.rong.mixinbooter.IEarlyMixinLoader;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
-public class MixinLoader implements IEarlyMixinLoader {
+public class MixinLoader implements IEarlyMixinLoader, IFMLLoadingPlugin {
 	@Override
 	public List<String> getMixinConfigs() {
 		ArrayList<String> ret = new ArrayList<>();
 		ret.add("mixins.minefantasyreforged.json");
 		return ret;
 	}
+
+	@Override
+	String[] getASMTransformerClass(){
+		return null;
+	}
+
+	@Override
+	String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	void injectData(Map<String, Object> var1) {}
+
+	@Override
+	String getAccessTransformerClass() {
+		return null;
+	}
+
 }
